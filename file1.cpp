c018a7f70eec065f3c4a097074af94117f40a1c4
Gaurav Gupta <gauravgupta662454@gmail.com>
2025-07-26 22:25:41 +0530

    Add files via upload

---
+++ file1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int glo= 7;
+
+void sum ()
+{
+  
+
+  cout<<glo;
+
+
+}
+int main()
+{   // int glo=76;
+  // int a=5, b=87;
+  //  cout<<" Hello world\n";
+  //cout<<" The value of a is "<<a<<".\n The value of b is "<<b;
+  //  cout<<"\n The value of a+b is="<<a+b;
+    sum();
+    cout<<glo;
+    
+    
+    
+
+
+
+
+
+    return 0;
+
+
+
+
+
+}
