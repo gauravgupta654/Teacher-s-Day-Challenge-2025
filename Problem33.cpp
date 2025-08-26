7f331f26693423eccbf9f09badba2322662b0adf
Gaurav Gupta <gauravgupta662454@gmail.com>
2025-08-26 23:36:07 +0530

    Create Problem33.cpp

---
+++ Problem33.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string num1, num2;
+    cin >> num1 >> num2;
+    
+    for (int i = 0; i < num1.length(); i++) {
+        if (num1[i] != num2[i]) {
+            cout << '1';
+        } else {
+            cout << '0';
+        }
+    }
+    
+    return 0;
+}
